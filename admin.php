1e226bc203cf0bec5fc527cdc924f9c2aa23491c
blogfordevelopers <bikash.pal2000@gmail.com>
2014-10-19 18:04:53 +0530

    Initial Plugin Information added

---
+++ admin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  Plugin Name: Woocommerce Products Analytics
+  Plugin URI: http://www.blogfordeveloper.com/
+  Description: Track your products with extensive analytics.
+  Version: 1.0
+  Author: Ramen Dey & Bivash Kanti Pal
+  Author URI: http://www.blogfordeveloper.com/about-us/
+*/
+
+
+global $wpdb;
+
+?>
